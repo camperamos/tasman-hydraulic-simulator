b559e24aea6ab24814c7d4e903712dc99735f7c1
Camilo Perez <camperamos@gmail.com>
2026-04-28 15:06:21 +1000

    Fix app import for Streamlit Cloud

--- app/app.py
+++ app/app.py
@@ -2,9 +2,12 @@ import sys
 from pathlib import Path
 
 ROOT_DIR = Path(__file__).resolve().parents[1]
-sys.path.insert(0, str(ROOT_DIR))
+APP_DIR = Path(__file__).resolve().parent
 
-from app.ui import render_ui
+sys.path.insert(0, str(ROOT_DIR))
+sys.path.insert(0, str(APP_DIR))
+
+from ui import render_ui
 
 if __name__ == "__main__":
     render_ui()
